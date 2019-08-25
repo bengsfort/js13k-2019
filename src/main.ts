2fea457f6e3dfb506afc08048adc86af2fe61955
Taylor Thompson <jamestthompson3@gmail.com>
2019-08-25 11:27:24 +0300

    updates to drawing

--- src/main.ts
+++ src/main.ts
@@ -5,28 +5,19 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 const PIXEL_SIZE = 10;
 
-// prettier-ignore
-// tslint-ignore
-const simpleTile = [
-  0,0,0,0,1,1,0,1,
-  0,0,2,2,0,1,1,0,
-  0,2,1,0,0,2,1,0,
-  0,2,2,0,0,0,1,0
-]
-
 const getIndex = (row, column) => row * width + column;
+enum PixelType {
+  EMPTY = 0,
+  DEEP = 1,
+  NEAR = 2,
+}
+
+// Change space background
 function shuffle(a) {
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
   }
-  return a;
-}
-
-enum PixelType {
-  EMPTY = 0,
-  DEEP = 1,
-  NEAR = 2,
 }
 
 function generateMap() {
@@ -38,7 +29,7 @@ function generateMap() {
   return map;
 }
 
-const map = generateMap();
+let map = generateMap();
 const drawPixel = type => {
   for (let row = 0; row < height; row++) {
     for (let col = 0; col < width; col++) {
@@ -62,11 +53,8 @@ const drawSatellite = () => {
   for (let i = 0; i < 10; i++) {
     for (let row = 0; row < 4; row++) {
       const rowOffset = i === 0 ? 0 : 4 * i;
-      // simpleTile rows
       for (let col = 0; col < 6; col++) {
         const colOffset = i === 0 ? 0 : 6 * i;
-        //console.log(colOffset, rowOffset, index);
-        // (row + rowOffset - col) * PIXEL_SIZE, cool 3d effect maybe use for space-ship animation
         context.fillRect(
           (col + colOffset) * PIXEL_SIZE,
           (row + rowOffset - col) * PIXEL_SIZE,
@@ -85,8 +73,6 @@ const context: CanvasRenderingContext2D = canvas.getContext("2d");
 // @todo: This needs to update on resize.
 canvas.width = width;
 canvas.height = height;
-canvas.style.background = "#000";
-
 document.body.appendChild(canvas);
 
 // Instantiate our main loop.
@@ -119,27 +105,23 @@ function drawPlanet() {
   context.arc(width / 4, height, 240, Math.PI, 0);
   context.fill();
 }
+
 drawSpace();
 drawPlanet();
 drawSatellite();
+let travelling = false;
 
-// let travelling = true;
-// setTimeout(() => { travelling = false;
-//   drawPlanet();
-// }, 2000);
-
-// go
-(function main() {
+(function main(tFrame) {
   animFrame = window.requestAnimationFrame(main);
-  const now = performance.now();
-  const elapsed = now - prevFrame;
-  // if (travelling) {
-  //   drawSpace();
-  // }
+  const elapsed = tFrame - prevFrame;
 
   if (elapsed >= FPS) {
     updateSquareBoy();
-    prevFrame = now;
+    if (travelling) {
+      shuffle(map);
+      drawSpace();
+    }
+    prevFrame = performance.now();
   }
 
   if (isPressed.esc) {
